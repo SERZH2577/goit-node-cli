023e4c93e688313217314482b708a6fae9d58ca5
Serhii Plis <serzh2577@gmail.com>
2024-04-18 01:19:38 +0300

    add

--- contacts.js
+++ contacts.js
@@ -1,4 +1,32 @@
 const fs = require("node:fs/promises");
 const path = require("node:path");
 
-// const contactsPath =
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = listContacts();
+  const result = contacts.find((contact) => (contact.id = contactId));
+
+  return result || null;
+}
+
+async function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту (з id).
+}
+
+// console.log(getContactById("AeHIrLTr6JkxGE6SN-0Rwq"));
+
+module.exports = {
+  listContacts,
+  getContactById,
+};
--- index.js
+++ index.js
@@ -1,37 +1,43 @@
-import { program } from "commander";
-program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
+// import { program } from "commander";
+// program
+//   .option("-a, --action <type>", "choose action")
+//   .option("-i, --id <type>", "user id")
+//   .option("-n, --name <type>", "user name")
+//   .option("-e, --email <type>", "user email")
+//   .option("-p, --phone <type>", "user phone");
 
-program.parse();
+// program.parse();
 
-const options = program.opts();
+// const options = program.opts();
+
+const contacts = require("./contacts");
+
+// console.log(contacts.listContacts());
 
 // TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      const allContacts = await contacts.listContacts();
+      // return console.log(allContacts);
       break;
 
     case "get":
-      // ... id
+      const oneContact = await contacts.getContactById();
+      // return console.log(oneContact);
       break;
 
-    case "add":
-      // ... name email phone
-      break;
+    // case "add":
+    //   // ... name email phone
+    //   break;
 
-    case "remove":
-      // ... id
-      break;
+    // case "remove":
+    //   // ... id
+    //   break;
 
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
 }
 
-invokeAction(options);
+invokeAction({ action: "list" });
